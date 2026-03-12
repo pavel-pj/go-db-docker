c05442a8e1dfbae086548f98aa929f9eaadba620
Pavel <voroshilov.pavel@gmail.com>
2026-03-12 08:51:47 +0300

    context

--- backend/main.go
+++ backend/main.go
@@ -10,49 +10,24 @@ func worker(ctx context.Context, id int) {
 	for {
 		select {
 		case <-ctx.Done():
-			fmt.Println("worker", id, "остановлен:", ctx.Err())
+			fmt.Println("worker:", id, "завершил работу")
 			return
 		default:
-			fmt.Println("worker:", id, "работает")
+			fmt.Println("worker:", id, "Работает")
 			time.Sleep(100 * time.Millisecond)
 		}
 	}
 }
 
 func main() {
-	ctx, cancel := context.WithTimeout(context.Background(), 4*time.Second)
-	defer cancel()
+	ctx, cancel := context.WithCancel(context.Background())
 
-	for x := 1; x < 3; x++ {
-		go worker(ctx, x)
+	for i := 1; i < 5; i++ {
+		go worker(ctx, i)
 	}
 
-	time.Sleep(6 * time.Second)
+	time.Sleep(2 * time.Second)
+	cancel()
+	time.Sleep(200 * time.Millisecond)
 
 }
-
-/*
-func worker(ctx context.Context, id int) {
-	for {
-		select {
-		case <-ctx.Done(): // сигнал отмены или дедлайна
-			fmt.Println("worker", id, "остановлен:", ctx.Err())
-			return // корректное завершение горутины
-		default:
-			fmt.Println("worker", id, "работает")
-			time.Sleep(100 * time.Millisecond) // имитация полезной работы
-		}
-	}
-}
-
-func main() {
-	// Контекст сам отменится через 2 секунды
-	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
-	defer cancel() // ВСЕ РАВНО НУЖНО ВЫЗВАТЬ! (освобождает ресурсы)
-
-	go worker(ctx, 1)
-
-	// Можно даже не вызывать cancel() вручную - таймаут сработает сам
-	time.Sleep(3 * time.Second)
-}
-*/
