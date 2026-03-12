fe24da9cd52f74eadd6d841052a8d905664e3b1f
Pavel <voroshilov.pavel@gmail.com>
2026-03-12 08:46:40 +0300

    fix

--- backend/main.go
+++ backend/main.go
@@ -9,6 +9,32 @@ import (
 func worker(ctx context.Context, id int) {
 	for {
 		select {
+		case <-ctx.Done():
+			fmt.Println("worker", id, "остановлен:", ctx.Err())
+			return
+		default:
+			fmt.Println("worker:", id, "работает")
+			time.Sleep(100 * time.Millisecond)
+		}
+	}
+}
+
+func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), 4*time.Second)
+	defer cancel()
+
+	for x := 1; x < 3; x++ {
+		go worker(ctx, x)
+	}
+
+	time.Sleep(6 * time.Second)
+
+}
+
+/*
+func worker(ctx context.Context, id int) {
+	for {
+		select {
 		case <-ctx.Done(): // сигнал отмены или дедлайна
 			fmt.Println("worker", id, "остановлен:", ctx.Err())
 			return // корректное завершение горутины
@@ -29,3 +55,4 @@ func main() {
 	// Можно даже не вызывать cancel() вручную - таймаут сработает сам
 	time.Sleep(3 * time.Second)
 }
+*/
