ef4b293a575fc22e115c923996547a8a0619871c
Pavel <voroshilov.pavel@gmail.com>
2026-03-16 20:39:17 +0300

    pipelines

--- backend/main.go
+++ backend/main.go
@@ -1,79 +1,56 @@
 package main
 
-import (
-	"fmt"
-	"strconv"
-	"sync"
-	"time"
-)
+import "fmt"
 
-type Job struct {
-	ID  int
-	Val string
-}
+func get(nums ...int) <-chan int {
 
-type Result struct {
-	ID     int
-	Output string
-	Err    error
-}
+	out := make(chan int)
 
-func worker(id int, jobs <-chan Job, result chan<- Result, wg *sync.WaitGroup) {
-	defer wg.Done()
-
-	for val := range jobs {
-		time.Sleep(100 * time.Millisecond)
-		fmt.Println("Worker:", id, "Запись ID:", val.ID)
-		result <- Result{
-			ID:     val.ID,
-			Output: val.Val,
-			Err:    nil,
+	go func() {
+		defer close(out)
+		for _, n := range nums {
+			out <- n
 		}
-	}
+	}()
 
+	return out
+
+}
+
+func getSquares(in <-chan int) <-chan int {
+	out := make(chan int)
+
+	go func() {
+		defer close(out)
+		for val := range in {
+			out <- val * val
+		}
+	}()
+
+	return out
+}
+
+func double(in <-chan int) <-chan int {
+	out := make(chan int)
+
+	go func() {
+		defer close(out)
+		for v := range in {
+			out <- v * 2
+		}
+	}()
+
+	return out
 }
 
 func main() {
-	var wg sync.WaitGroup
-	workers := 4
 
-	jobs := make(chan Job, 16)
-	results := make(chan Result, 16)
+	nums := get(1, 2, 3, 4, 5)
+	squares := getSquares(nums)
+	dbl := double(squares)
 
-	wg.Add(workers)
-	for i := 0; i < workers; i++ {
-		go worker(i, jobs, results, &wg)
-	}
-
-	go func() {
-		for x := 0; x < 45; x++ {
-			jobs <- Job{
-				ID:  x,
-				Val: fmt.Sprintf("Значение Задачи : %s", strconv.Itoa(x)),
-			}
-		}
-		close(jobs)
-	}()
-
-	var resultWg sync.WaitGroup
-	resultInfo := []Result{}
-
-	resultWg.Add(1)
-
-	go func() {
-		defer resultWg.Done()
-		for res := range results {
-			resultInfo = append(resultInfo, res)
-		}
-	}()
-
-	wg.Wait()
-	close(results)
-	resultWg.Wait()
-
-	fmt.Println("\n=== ВСЕ РЕЗУЛЬТАТЫ ===")
-	for _, res := range resultInfo {
-		fmt.Printf("РЕЗУЛЬТАТ: id: %02d, Значение : %s\n", res.ID, res.Output)
+	for v := range dbl {
+		fmt.Println(v)
 	}
 
 }
