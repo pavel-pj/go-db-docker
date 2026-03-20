9f470803c766940d4d95c7da145617b43afc7179
Pavel <voroshilov.pavel@gmail.com>
2026-03-20 11:03:19 +0300

    atomic

--- backend/main.go
+++ backend/main.go
@@ -3,52 +3,26 @@ package main
 import (
 	"fmt"
 	"sync"
-	"time"
+	"sync/atomic"
 )
 
-func worker(id int, jobs <-chan int, results chan<- int) {
-
-	time.Sleep(100 * time.Millisecond)
-	for job := range jobs {
-		result := job * 2
-		results <- result
-		fmt.Println("worker :", id, "job:", job, "result:", result)
-	}
-
-}
+var counter int64
+var wg sync.WaitGroup
 
 func main() {
 
-	var wg sync.WaitGroup
-	limit := 10
-	jobs := make(chan int)
-	results := make(chan int)
-
-	for i := 0; i < limit; i++ {
+	for x := 0; x < 100; x++ {
 		wg.Add(1)
-
-		go func(id int) {
+		go func(idx int) {
 			defer wg.Done()
-			worker(id, jobs, results)
-		}(i)
-
+			for i := 0; i < 10000; i++ {
+				atomic.AddInt64(&counter, 1)
+				fmt.Println(atomic.LoadInt64(&counter))
+			}
+		}(x)
 	}
 
-	go func() {
-
-		for x := 0; x < 100; x++ {
-			jobs <- x
-		}
-		close(jobs)
-	}()
-
-	go func() {
-		wg.Wait()
-		close(results)
-	}()
-
-	for result := range results {
-		fmt.Println("RESULT:", result)
-	}
+	wg.Wait()
+	fmt.Println("total:", atomic.LoadInt64(&counter))
 
 }
